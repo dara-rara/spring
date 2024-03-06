c0ccb402d1b1fa136c425df389ed4a7ecbd0a3af
dara-rara <glebova555dasha@gmail.com>
2024-03-06 21:12:41 +0500

    поправки2

--- src/main/java/com/example/demo/service/ServiceClothing.java
+++ src/main/java/com/example/demo/service/ServiceClothing.java
@@ -1,11 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.service.common.Blouse;
-import com.example.demo.service.common.Dress;
-import com.example.demo.service.common.Trousers;
+import com.example.demo.service.common.Clothing;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,17 +12,18 @@ public class ServiceClothing {
     //ошибка с аннотацией @Log4j2 в IDEA Community
     private static final org.apache.logging.log4j.Logger log =
             org.apache.logging.log4j.LogManager.getLogger(ServiceClothing.class);
-    private final Blouse blouse;
+    private final Clothing blouse;
     @Autowired
-    private Dress dress;
-    private Trousers trousers;
+    @Qualifier("dress")
+    private Clothing dress;
+    private Clothing trousers;
 
-    public ServiceClothing(Blouse blouse) {
+    public ServiceClothing(@Qualifier("blouse") Clothing blouse) {
         this.blouse = blouse;
     }
 
     @Autowired
-    public void setTrousers(Trousers trousers) {
+    public void setTrousers(@Qualifier("trousers") Clothing trousers) {
         this.trousers = trousers;
     }
 
